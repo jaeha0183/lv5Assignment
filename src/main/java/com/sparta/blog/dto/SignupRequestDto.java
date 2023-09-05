c0b746db22aa62457c31aa1709a6cd4aeee4ed78
Jaeha Lee <jaeha0183@gmail.com>
2023-09-05 19:19:32 +0900

    관리자 기능 추가, 비밀번호 조건 수정

--- src/main/java/com/sparta/blog/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/blog/dto/SignupRequestDto.java
@@ -12,7 +12,7 @@ public class SignupRequestDto {
     @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "아이디 형식 오류")
     @NotBlank
     private String username;
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$", message = "비밀번호 형식 오류")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=!]).{8,15}$", message = "비밀번호 형식 오류")
     @NotBlank
     private String password;
     private boolean admin = false;
